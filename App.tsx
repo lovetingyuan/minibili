65db5775b89e041908e5c07ff8f67461cfa6dc3a
tingyuan <1932294867@qq.com>
2023-03-27 17:23:55 +0800

    chore

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import App from './routes/Index'
 import * as Sentry from 'sentry-expo'
 
 Sentry.init({
-  dsn: 'https://39c1a6b78a794c479d6b716223e91f7b@o58488.ingest.sentry.io/125390',
+  dsn: 'https://2ee085cec3774459876d706eac0fe6a5@o58488.ingest.sentry.io/4504039187742720',
+  // dsn: 'https://39c1a6b78a794c479d6b716223e91f7b@o58488.ingest.sentry.io/125390',
   enableInExpoDevelopment: false,
   debug: __DEV__, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
 })
