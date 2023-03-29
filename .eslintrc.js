09d5880edff38f0b0be9f26f9018839450af648b
tingyuan <1932294867@qq.com>
2023-03-29 17:14:02 +0800

    chore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,7 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: '@react-native-community',
   rules: {
     semi: 'off',
   },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-        'react-hooks/exhaustive-deps': 'warn',
-      },
-    },
-  ],
 }
