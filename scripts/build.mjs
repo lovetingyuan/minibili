5d522ab7d0861f6228dfb906d91fad28a9ad5555
tingyuan <1932294867@qq.com>
2023-04-12 15:08:31 +0800

    fix: 修复构建问题

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -3,14 +3,14 @@
 const { version } = require('../package.json')
 const newVersion = await question(`更新版本（${version} -> ?）`)
 const semver = require('semver')
-if (!semver.valid(newVersion) || !semver.gt(newVersion, version)) {
+if (!semver.valid(newVersion) || semver.lt(newVersion, version)) {
   throw new Error('版本号输入错误')
 }
 const changes = await question('更新日志（使用双空格分开）')
 if (!changes.trim()) {
   throw new Error('更新日志不能为空')
 }
-await $`npm version ${newVersion} -m ${changes}`
+await $`npm version ${newVersion} -m ${changes} --allow-same-version`
 const { expo } = await fs.readJson(path.resolve(__dirname, '../app.json'))
 expo.version = newVersion
 expo.ios.buildNumber = newVersion
