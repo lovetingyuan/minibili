a610396d70a09a0a8745e3194cb63e1d215cca97
lovetingyuan <1932294867@qq.com>
2023-05-10 23:06:12 +0800

    chore

--- src/routes/Dynamic/PGCItem.tsx
+++ src/routes/Dynamic/PGCItem.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { View, Text, Image } from 'react-native'
 import { DynamicItemType } from '../../api/dynamic-items'
-// import RichText from '../../components/RichText'
 import { HandledDynamicTypeEnum } from '../../api/dynamic-items.type'
 
 export default function MusicItem(
   props: DynamicItemType<HandledDynamicTypeEnum.DYNAMIC_TYPE_PGC>,
 ) {
   return (
-    <View>
+    <View style={{ gap: 10 }}>
       <Text style={{ fontWeight: 'bold' }}>{props.payload.title}</Text>
       <Text>{props.payload.text}</Text>
       <Image
         source={{ uri: props.payload.cover }}
         style={{
-          width: 120,
+          width: 150,
+          height: 70,
         }}
       />
     </View>
