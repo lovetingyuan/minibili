2a9a2e6ff5d383d1472783e2e6530013ff666159
tingyuan <1932294867@qq.com>
2023-05-31 11:33:44 +0800

    fix: dark mode

--- src/hooks/useIsDark.ts
+++ src/hooks/useIsDark.ts
@@ -1,15 +1,16 @@
 import { useColorScheme } from 'react-native'
-import useUpdate from './useUpdate'
+// import useUpdate from './useUpdate'
 import { Appearance } from 'react-native'
+import React from 'react'
 
 export default function useIsDark() {
-  const color = useColorScheme()
-  const update = useUpdate()
+  const [color, setColor] = React.useState(useColorScheme())
   // return true
   const color2 = Appearance.getColorScheme()
-  if (color2 !== color) {
-    update()
-    return color2 === 'dark'
-  }
+  React.useEffect(() => {
+    if (color2 !== color) {
+      setColor(color2)
+    }
+  }, [color2, color])
   return color === 'dark'
 }
