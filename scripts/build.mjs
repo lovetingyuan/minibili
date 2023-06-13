4cd581ca4476512e3bce21c7f34141d39844e33f
tingyuan <1932294867@qq.com>
2023-06-13 18:39:42 +0800

    chore[skip ci]

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -83,93 +83,98 @@ await $`npm version ${newVersion}-${
 
 echo(
   chalk.cyan(
-    'eas: https://expo.dev/accounts/tingyuan/projects/minibili/builds',
+    'eas building: https://expo.dev/accounts/tingyuan/projects/minibili/builds',
   ),
 )
 
 try {
   await spinner('eas building...', async () => {
-    const res =
-      await $`eas build --platform android --profile production --json --non-interactive`
-    const buildList = getBuildList(res)
-    if (buildList[0].appVersion !== newVersion) {
-      throw new Error('构建未成功')
-    }
+    await $`eas build --platform android --profile production --json --non-interactive`
+    // const buildList = getBuildList(res)
+    // if (buildList[0].appVersion !== newVersion) {
+    //   throw new Error(
+    //     'Something wrong with the build, version does not match ' +
+    //       buildList[0].appVersion +
+    //       ' vs ' +
+    //       newVersion,
+    //   )
+    // }
   })
+  echo(chalk.blue('check eas build list...'))
+  await new Promise(r => {
+    setTimeout(r, 3000)
+  })
+  let buildListStr = ''
+
+  try {
+    buildListStr = await spinner('get eas build list...', () =>
+      retry(
+        5,
+        () =>
+          $`eas build:list --platform android --limit 5 --json --non-interactive --status finished --channel production`,
+      ),
+    )
+  } catch (err) {
+    echo(chalk.red('Failed to get build list.'))
+    throw err
+  }
+
+  const buildList = getBuildList(buildListStr)
+  if (buildList[0].appVersion !== newVersion) {
+    throw new Error('EAS latest version is not same as updated version.')
+  }
+
+  echo(chalk.blue('writing version file...'))
+
+  await fs.outputFile(
+    path.resolve(__dirname, '../docs/version.json'),
+    buildList.map(item => {
+      return item
+      // return {
+      //   version: item.appVersion,
+      //   changelog: item.gitCommitMessage.split('  '),
+      //   date: item.createdAt,
+      // }
+    }),
+  )
+
+  echo(chalk.blue('download apk file...'))
+
+  try {
+    await $`rm -rf apk && mkdir -p apk`
+    await retry(
+      3,
+      () =>
+        $`wget ${buildList[0].artifacts.buildUrl} -q -O ./apk/minibili-${buildList[0].appVersion}.apk`,
+    )
+  } catch (err) {
+    echo(chalk.red('Failed to download latest apk.'))
+    throw err
+  }
+
+  echo(chalk.blue('publish to npm...'))
+
+  try {
+    await retry(3, () => $`npm publish --registry=https://registry.npmjs.org/`)
+  } catch (err) {
+    echo(chalk.red('Failed to publish to npm.'))
+    throw err
+  }
+
+  echo(chalk.blue('commit to github...'))
+
+  try {
+    await $`git commit -a --amend -C HEAD`
+    await spinner('git push...', () => retry(5, () => $`git push`))
+  } catch (err) {
+    echo(chalk.red('Failed to push build list to git.'))
+    throw err
+  }
 } catch (err) {
   await $`git checkout -- .`
   await $`npm version ${version} -m "failed to publish ${newVersion}" --allow-same-version`
   echo(chalk.red('Failed to build new apk on EAS.'))
   process.exit(-1)
 }
-let buildListStr = ''
 
-await new Promise(r => {
-  setTimeout(r, 3000)
-})
-
-try {
-  buildListStr = await spinner('get eas build list...', () =>
-    retry(
-      5,
-      () =>
-        $`eas build:list --platform android --limit 5 --json --non-interactive --status finished --channel production`,
-    ),
-  )
-} catch (err) {
-  echo(chalk.red('Failed to get build list.'))
-  throw err
-}
-
-const buildList = getBuildList(buildListStr)
-if (buildList[0].appVersion !== newVersion) {
-  throw new Error('EAS latest version is not same as updated version.')
-}
-
-await fs.outputFile(
-  path.resolve(__dirname, '../docs/version.json'),
-  buildList.map(item => {
-    return item
-    // return {
-    //   version: item.appVersion,
-    //   changelog: item.gitCommitMessage.split('  '),
-    //   date: item.createdAt,
-    // }
-  }),
-)
-
-try {
-  await $`rm -rf apk && mkdir -p apk`
-  await retry(
-    3,
-    () =>
-      $`wget ${buildList[0].artifacts.buildUrl} -q -O ./apk/minibili-${buildList[0].appVersion}.apk`,
-  )
-} catch (err) {
-  echo(chalk.red('Failed to download latest apk.'))
-  throw err
-}
-
-try {
-  await retry(3, () => $`npm publish --registry=https://registry.npmjs.org/`)
-} catch (err) {
-  echo(chalk.red('Failed to publish to npm.'))
-  throw err
-}
-
-try {
-  echo('commit to github...')
-  await $`git commit -a --amend -C HEAD`
-  await spinner('git push...', () => retry(5, () => $`git push`))
-  echo(chalk.green('Build done!'))
-} catch (err) {
-  echo(chalk.red('Failed to push build list to git.'))
-  throw err
-}
-
-// try {
-//   await $`expo-cli publish`
-// } catch (err) {
-//   echo(chalk.red('Failed to publish sourcemap.'))
-//   throw err
-// }
+echo(chalk.green('Build done!'))
