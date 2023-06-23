fa99a593f5c14a895ca902de13909d4262209ec0
lovetingyuan <1932294867@qq.com>
2023-06-23 20:37:23 +0800

    chore

--- src/routes/Play/VideoInfo.tsx
+++ src/routes/Play/VideoInfo.tsx
@@ -29,15 +29,15 @@ export default React.memo(function VideoInfo(props: {
       <View>
         <Text style={styles.videoTitle}>{title}</Text>
         {videoDesc ? <Text style={[styles.videoDesc]}>{videoDesc}</Text> : null}
-        {(videoInfo?.videosNum || 0) > 1 && videoInfo?.pages ? (
+        {(videoInfo.videosNum || 0) > 1 && videoInfo.pages ? (
           <ListItem.Accordion
             icon={<Icon name={'chevron-down'} type="material-community" />}
             containerStyle={styles.pagesTitle}
             content={
               <ListItem.Content>
                 <ListItem.Title>
-                  视频分集（{videoInfo?.videosNum}） {page}:{' '}
-                  {videoInfo?.pages[page - 1].title}
+                  视频分集（{videoInfo.videosNum}） {page}:{' '}
+                  {videoInfo.pages[page - 1].title}
                 </ListItem.Title>
               </ListItem.Content>
             }
@@ -45,7 +45,7 @@ export default React.memo(function VideoInfo(props: {
             onPress={() => {
               setExpanded(!expanded)
             }}>
-            {videoInfo?.pages.map(v => {
+            {videoInfo.pages.map(v => {
               const selected = v.page === page
               return (
                 <ListItem
@@ -71,8 +71,9 @@ export default React.memo(function VideoInfo(props: {
               )
             })}
           </ListItem.Accordion>
-        ) : videoInfo?.videos !== videoInfo?.videosNum ? (
-          <Text style={{ marginTop: 10, color: 'orange' }}>
+        ) : typeof videoInfo.videosNum === 'number' &&
+          videoInfo.videos !== videoInfo.videosNum ? (
+          <Text style={{ marginTop: 10, color: '#FF7F24' }}>
             该视频为交互视频，暂不支持
           </Text>
         ) : null}
