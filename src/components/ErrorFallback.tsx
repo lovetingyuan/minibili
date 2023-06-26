780714759b26f8e5ff79cac1a0bbb00a97db15ef
tingyuan <1932294867@qq.com>
2023-06-26 14:55:42 +0800

    chore

--- src/components/ErrorFallback.tsx
+++ src/components/ErrorFallback.tsx
@@ -11,18 +11,29 @@ import {
 import { StatusBar } from 'expo-status-bar'
 import MyImage from './MyImage'
 import { site } from '../constants'
+import useIsDark from '../hooks/useIsDark'
+
+class ExitError extends Error {
+  constructor(m: string) {
+    super(m)
+    this.name = 'IgnoreThisError'
+  }
+}
 
 export default function ErrorFallback(props: { message?: string }) {
+  // const [, seta] = React.useState(null)
   React.useEffect(() => {
     showFatalError()
     const handleBack = () => {
-      BackHandler.exitApp()
-      return true
+      throw new ExitError('😢')
+      // BackHandler.exitApp()
+      // return true
     }
     BackHandler.addEventListener('hardwareBackPress', handleBack)
   }, [])
+  const dark = useIsDark()
   return (
-    <View>
+    <View style={[dark ? { backgroundColor: '#333' } : {}, { flex: 1 }]}>
       <StatusBar style="auto" />
       <MyImage source={require('../../assets/error.png')} widthScale={0.8} />
       <Text style={styles.errorText}>
@@ -32,22 +43,25 @@ export default function ErrorFallback(props: { message?: string }) {
         {'\n\n'}
         我们会处理这个错误，感谢您的理解和支持
         {'\n\n'}
-        您可以退出应用并重新打开{'\n'}我们推荐您{'  '}
+        您可以退出应用并重新打开，我们推荐您安装新版
+      </Text>
+      <View style={styles.downloadBtn}>
         <Button
           title="下载最新版本"
           onPress={() => {
             Linking.openURL(site)
           }}
         />
-      </Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   errorText: {
-    color: 'red',
+    color: '#ff4834',
     marginHorizontal: 30,
     fontSize: 16,
   },
+  downloadBtn: { marginVertical: 30, paddingHorizontal: 30 },
 })
