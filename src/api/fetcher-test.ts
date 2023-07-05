b464166fbe2a218a4cbcd778bd18bb6b4a4a0319
tingyuan <1932294867@qq.com>
2023-07-05 14:02:40 +0800

    chore[skip ci]

--- src/api/fetcher-test.ts
+++ src/api/fetcher-test.ts
@@ -42,84 +42,103 @@
 const UA =
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
 
-// function getCookie() {
-//   // return 'C4C1FACA-CCF4-AA5B-61F0-93D154A0979F97177infoc'
-//   return fetch('https://api.bilibili.com/x/frontend/finger/spi', {
-//     headers: {
-//       accept: '*/*',
-//       'accept-language': 'zh-CN,zh;q=0.9',
-//       'cache-control': 'no-cache',
-//       pragma: 'no-cache',
-//       'sec-ch-ua':
-//         '"Not.A/Brand";v="8", "Chromium";v="114", "Microsoft Edge";v="114"',
-//       'sec-ch-ua-mobile': '?0',
-//       'sec-ch-ua-platform': '"macOS"',
-//       'sec-fetch-dest': 'empty',
-//       'sec-fetch-mode': 'cors',
-//       'sec-fetch-site': 'same-origin',
-//     },
-//     referrer: 'https://api.bilibili.com/x/frontend/finger/spi',
-//     referrerPolicy: 'strict-origin-when-cross-origin',
-//     body: null,
-//     method: 'GET',
-//     mode: 'cors',
-//     credentials: 'omit',
-//   })
-//     .then(res => res.json())
-//     .then(res => {
-//       return `buvid3=${res.data.b_3}; buvid4=${res.data.b_4}`
-//     })
-//   // return fetch('https://api.bilibili.com/x/web-interface/search/default', {
-//   //   headers: {
-//   //     accept:
-//   //       'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-//   //     'accept-language': 'zh-CN,zh;q=0.9',
-//   //     'cache-control': 'no-cache',
-//   //     pragma: 'no-cache',
-//   //     'user-agent': UA,
-//   //   },
-//   //   referrerPolicy: 'no-referrer-when-downgrade',
-//   //   body: null,
-//   //   method: 'GET',
-//   //   mode: 'cors',
-//   //   credentials: 'omit',
-//   // })
-//   //   .then(response => {
-//   //     const cookie = response.headers.get('Set-Cookie') || ''
-//   //     if (cookie) {
-//   //       // console.log(222, cookie)
-//   //       const cookies = cookie.split('; ')
-//   //       const id = cookies.find(v => v.startsWith('buvid3='))
-//   //       return id
-//   //     }
-//   //   })
-//   //   .catch(() => {
-//   //     return ''
-//   //   })
-// }
+function getCookie(mid: string) {
+  // return 'C4C1FACA-CCF4-AA5B-61F0-93D154A0979F97177infoc'
+  // return fetch('https://api.bilibili.com/x/frontend/finger/spi', {
+  //   headers: {
+  //     accept: '*/*',
+  //     'accept-language': 'zh-CN,zh;q=0.9',
+  //     'cache-control': 'no-cache',
+  //     pragma: 'no-cache',
+  //     'sec-ch-ua':
+  //       '"Not.A/Brand";v="8", "Chromium";v="114", "Microsoft Edge";v="114"',
+  //     'sec-ch-ua-mobile': '?0',
+  //     'sec-ch-ua-platform': '"macOS"',
+  //     'sec-fetch-dest': 'empty',
+  //     'sec-fetch-mode': 'cors',
+  //     'sec-fetch-site': 'same-origin',
+  //   },
+  //   referrer: 'https://api.bilibili.com/x/frontend/finger/spi',
+  //   referrerPolicy: 'strict-origin-when-cross-origin',
+  //   body: null,
+  //   method: 'GET',
+  //   mode: 'cors',
+  //   credentials: 'omit',
+  // })
+  //   .then(res => res.json())
+  //   .then(res => {
+  //     const id = `buvid4=${res.data.b_4.replaceAll('=', '%3D')}; buvid3=${
+  //       res.data.b_3
+  //     }`
+  //     console.log(1234, id)
+  //     return id
+  //   })
+  return fetch('https://space.bilibili.com/' + mid + '/dynamic', {
+    headers: {
+      accept:
+        'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+      'accept-language': 'zh-CN,zh;q=0.9',
+      'cache-control': 'no-cache',
+      pragma: 'no-cache',
+      'user-agent': UA,
+      'sec-ch-ua':
+        '"Not.A/Brand";v="8", "Chromium";v="114", "Microsoft Edge";v="114"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"macOS"',
+      'sec-fetch-dest': 'document',
+      'sec-fetch-mode': 'navigate',
+      'sec-fetch-site': 'same-origin',
+      'sec-fetch-user': '?1',
+      'upgrade-insecure-requests': '1',
+    },
+    referrer: 'https://www.bilibili.com/',
+    referrerPolicy: 'no-referrer-when-downgrade',
+    body: null,
+    method: 'GET',
+    mode: 'cors',
+    credentials: 'omit',
+  })
+    .then(response => {
+      const cookie = response.headers.get('Set-Cookie') || ''
+      if (cookie) {
+        // console.log(222, cookie)
+        const cookies = cookie.split('; ')
+        const id = cookies.find(v => v.startsWith('buvid3='))
+        return id || ''
+      }
+      return ''
+    })
+    .catch(() => {
+      return ''
+    })
+}
 
-// let cookie = ''
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+let cookie = ''
+// getCookie()
 
 export default async function request<D extends any>(url: string) {
   const requestUrl = url.startsWith('http')
     ? url
     : 'https://api.bilibili.com' + url
-  // const { searchParams } = new URL(requestUrl)
-  // const isDynamic = url.includes('/x/polymer/web-dynamic/v1/feed/space')
+  const { searchParams } = new URL(requestUrl)
+  const isDynamic = url.includes('/x/polymer/web-dynamic/v1/feed/space')
   // if (isDynamic) {
   //   mid = searchParams.get('host_mid') || mid
   // }
-  // if (!cookie && isDynamic) {
-  //   cookie = await getCookie()
-  //   console.log(1234, cookie)
-  // }
+  if (isDynamic) {
+    cookie = await getCookie(searchParams.get('host_mid')!)
+    // console.log(1234, cookie)
+  }
   return fetch(requestUrl + '&_t=' + Date.now(), {
     headers: {
       accept: 'application/json, text/plain, */*',
       'accept-language': 'zh-CN,zh;q=0.9',
       'cache-control': 'no-cache',
       pragma: 'no-cache',
-      cookie: 'buvid3=CA278FEF-3518-0489-D2A5-146228412E4217147infoc',
+      // cookie,
+      cookie: 'buvid3=CED70C29-758A-B176-7961-8F8EFE2E651D82318infoc',
+      // CED70C29-758A-B176-7961-8F8EFE2E651D82318infoc
       'user-agent': UA,
     },
     // referrer: 'https://space.bilibili.com',
