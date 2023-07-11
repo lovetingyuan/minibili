f4f41ba20b0ca5583e17beed8f0768a6354687da
tingyuan <hbtwhucsis@gmail.com>
2023-07-11 13:43:09 +0800

    feat: test cookie [skip ci]

---
+++ src/api/get-cookie.test.ts
@@ -0,0 +1,7 @@
+import { test, assert } from 'vitest'
+import getCookie from './get-cookie'
+
+test('get-cookie', async () => {
+  const res = await getCookie()
+  assert(/buvid3=(.+); _uuid=(.+); buvid4=(.+)/.test(res))
+})
