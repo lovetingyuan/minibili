93283a3bd7af9b9d47fd0108b8eae489b53de392
tingyuan <hbtwhucsis@gmail.com>
2024-01-08 14:39:30 +0800

    -

--- src/api/get-config.test.ts
+++ src/api/get-config.test.ts
@@ -2,7 +2,7 @@ import { test } from 'vitest'
 import { getRemoteConfig } from './get-config'
 import { ConfigSchema } from './get-config.schema'
 
-test('get-remote-config', async () => {
+test.skip('get-remote-config', async () => {
   const res = await getRemoteConfig()
   ConfigSchema.parse(res)
 }, 20000)
