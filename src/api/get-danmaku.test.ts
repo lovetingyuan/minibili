db1b0efbccd76c64a0fb97a7e4406e3f9d2918e5
tingyuan <hbtwhucsis@gmail.com>
2024-03-16 17:31:59 +0800

    [skip-ci]

--- src/api/get-danmaku.test.ts
+++ src/api/get-danmaku.test.ts
@@ -1,31 +1,31 @@
-import * as protobuf from 'protobufjs'
-import { assert, test } from 'vitest'
+// import * as protobuf from 'protobufjs'
+// import { assert, test } from 'vitest'
 
-import dm from '../constants/dm'
-import { DanmakuSchema } from './get-danmaku.schema'
+// import dm from '../constants/dm'
+// import { DanmakuSchema } from './get-danmaku.schema'
 
-const root = protobuf.Root.fromJSON(dm)
+// const root = protobuf.Root.fromJSON(dm)
 
-const lp = root.lookupType('DmSegMobileReply')
+// const lp = root.lookupType('DmSegMobileReply')
 
-test('get-danmaku', async () => {
-  const arrayBuffer = await fetch(
-    'https://api.bilibili.com/x/v2/dm/web/seg.so?type=1&oid=1459734495&pid=1001385310&segment_index=1',
-  ).then(res => {
-    return res.arrayBuffer()
-  })
+// test('get-danmaku', async () => {
+//   const arrayBuffer = await fetch(
+//     'https://api.bilibili.com/x/v2/dm/web/seg.so?type=1&oid=1459734495&pid=1001385310&segment_index=1',
+//   ).then(res => {
+//     return res.arrayBuffer()
+//   })
 
-  const bytes = new Uint8Array(arrayBuffer)
+//   const bytes = new Uint8Array(arrayBuffer)
 
-  const message = lp.decode(bytes)
-  const objects = lp.toObject(message, {
-    // bool: Boolean,
-    longs: Number,
-    enums: Number,
-    bytes: String,
-    // Object: String,
-  })
-  assert.ok(Array.isArray(objects.elems))
-  DanmakuSchema.parse(objects.elems[0])
-  DanmakuSchema.parse(objects.elems[10])
-})
+//   const message = lp.decode(bytes)
+//   const objects = lp.toObject(message, {
+//     // bool: Boolean,
+//     longs: Number,
+//     enums: Number,
+//     bytes: String,
+//     // Object: String,
+//   })
+//   assert.ok(Array.isArray(objects.elems))
+//   DanmakuSchema.parse(objects.elems[0])
+//   DanmakuSchema.parse(objects.elems[10])
+// })
